166a0f4090912f2fa8814ebd847d98f69ede8946
Brian Kardell <bkardell@gmail.com>
2024-07-02 16:30:16 -0400

    Make it easier to re-run last

--- manual-index.js
+++ manual-index.js
@@ -1,6 +1,12 @@
+const fs = require('fs')
 const index = require('./index.js').run
 
-//rerun index stuff with those
-//index(bookkeeping.previous.file, bookkeeping.latest.file)
+const bookkeeping = JSON.parse(
+	fs.readFileSync('./store/bookkeeping.json', 'utf8')
+)
 
-index("Jan 24 2024.json", "Jan 30 2024.json")
+//rerun index stuff with those
+index(bookkeeping.previous.file, bookkeeping.latest.file)
+
+// You can run this manually locally
+// with index("Jan 24 2024.json", "Jan 30 2024.json")

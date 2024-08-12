b6bbc1ce70e0fd29e1bc17e512f180de810c8ac7
Brian Kardell <bkardell@gmail.com>
2024-08-12 16:29:02 -0400

    minor rework to move collecting files into a reusable bit of API so that we can use it also for building the feed pretty easily with the last N

---
+++ entriesStore.js
@@ -0,0 +1,28 @@
+const fs = require('fs')
+const index = require('./index.js').run
+
+function stripFileExtension(str) {
+  return str.split(".")[0]
+}
+
+exports.getSortedListOfHTMLEntries = function (path) {
+  let files = fs.readdirSync(path)
+
+  files = files.filter(name => name.endsWith(".html") && name !== "index.html")
+  files.sort(function(a,b){
+    return new Date(stripFileExtension(a)) - new Date(stripFileExtension(b));
+  })
+  return files
+}
+
+exports.getSortedListOfJSONEntries = function (path) {
+  return exports.getSortedListOfHTMLEntries(path)
+  			.map(name => { 
+  				return name.replace('.html', '.json').replaceAll('-', ' ') 
+  			})
+}
+
+
+
+
+

ce79ca3cfb7d36f28b68938ee84e5dbd1dc41b55
Brian Kardell <bkardell@gmail.com>
2024-09-16 10:02:26 -0400

    tweak manual index to match new signatures

--- manual-index.js
+++ manual-index.js
@@ -1,12 +1,23 @@
 const fs = require('fs')
 const index = require('./index.js').run
+const utils = require('./utils.js')
 
 const bookkeeping = JSON.parse(
 	fs.readFileSync('./store/bookkeeping.json', 'utf8')
 )
 
+
+let m = 
+	utils.findNextMonday(
+		bookkeeping.latest.file.replace('.json', '')
+	)
+
 //rerun index stuff with those
-index(bookkeeping.previous.file, bookkeeping.latest.file)
+index(
+	bookkeeping.previous.file, 
+	bookkeeping.latest.file, 
+	utils.toISODateString(new Date())
+)
 
 // You can run this manually locally
 // with index("Jan 24 2024.json", "Jan 30 2024.json")

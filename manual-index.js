292afbedfdf93c04468af04961ab84e13acb9381
Brian Kardell <bkardell@gmail.com>
2024-09-16 12:42:43 -0400

    change manual index to use today if it is monday

--- manual-index.js
+++ manual-index.js
@@ -7,17 +7,34 @@ const bookkeeping = JSON.parse(
 )
 
 
-let m = 
-	utils.findNextMonday(
-		bookkeeping.latest.file.replace('.json', '')
-	)
+let today = new Date(),
+
+	// If today is monday, use today, otherwise fine previous monday
+	m = (today.getDay() == 1) ? today : utils.findPreviousMonday(today),
+	
+	previousReleaseDate = utils.dateFromISODateString(
+		utils.stripFileExtension(
+			bookkeeping.previous.file
+		)
+	),
+
+	latestReleaseDate = utils.dateFromISODateString(
+		utils.stripFileExtension(
+			bookkeeping.latest.file
+		)
+	),
+
+	diffReportDate = utils.findNextMonday(latestReleaseDate)
+
+	// if the relevant report day would be greater than today,
+	// the diff is just between latest and latest (ie, nothing)
+	if(diffReportDate < today) {
+		bookkeeping.previous = bookkeeping.latest
+	}
 
 //rerun index stuff with those
 index(
 	bookkeeping.previous.file, 
 	bookkeeping.latest.file, 
-	utils.toISODateString(new Date())
+	utils.toISODateString(m)
 )
-
-// You can run this manually locally
-// with index("Jan 24 2024.json", "Jan 30 2024.json")
